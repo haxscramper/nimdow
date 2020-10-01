2254e50befb39c624f5855490c82630154c6900f
haxscramper <haxscramper@gmail.com>
2020-10-02 00:33:24 +0300

    [WIP] Automatically send test key press

--- fswatch_rebuild.nim
+++ fswatch_rebuild.nim
@@ -1,7 +1,9 @@
 import libfswatch, libfswatch/fswatch
-import bitops, osproc, strtabs
+import bitops, osproc, strtabs, os
 import hmisc/helpers
 import hmisc/other/[hshell, pathwrap, oswrap, colorlogger]
+
+import x11/[xlib, xutil, x, keysym]
 
 startColorLogger()
 
@@ -53,14 +55,61 @@ let cmd = makeX11Cmd("Xephyr").withIt do:
   it.flag "ac"
   it.raw ":1"
 
-# let xephyr = startShell(cmd, {poEvalCommand, poParentStreams})
+if false:
+  let xephyr = startShell(cmd, {poEvalCommand, poParentStreams})
 
 setEnv("DISPLAY", ":1")
 
 let build = makeNimCmd("nimble").withIt do:
   it.cmd "build"
 
-let nimdow = makeX11Cmd("./nimdow")
+let nimdow = makeX11Cmd("./nimdow config.default.toml")
+
+var
+  display = XOpenDisplay(nil)
+  root: Window
+
+if display == nil:
+  err "Failed to to open display"
+else:
+  info "Display open op"
+
+
+let
+  screen = XDefaultScreen(display)
+  rootWindow = XRootWindow(display, screen)
+
+# var
+#   subw: ptr[Window]
+#   rootRet: Window
+#   parentWindow: Window
+#   cnt: cuint
+
+# let status = XQueryTree(
+#   display, rootWindow,
+#   addr rootRet, addr parentWindow, addr subw, addr cnt)
+
+# echo cnt
+
+
+proc makeEvent(): XKeyEvent =
+  result.display = display
+  result.x = 1
+  result.y = 1
+  result.xRoot = 1
+  result.yRoot = 1
+  result.sameScreen = XBool(1)
+  result.theType = KeyPress
+  result.keycode = cuint(XK_2)
+  result.state = cuint(XK_Super_L)
+
+proc sendEvents =
+  var ev = makeEvent()
+  discard XSendEvent(display, rootWindow, XBool(1),
+                     ButtonPressMask, cast[ptr XEvent](addr ev))
+
+  info "Events send"
+
 
 var nimdowProc: Process
 
@@ -76,6 +125,8 @@ proc updateRun =
     nimdowProc.close()
 
   nimdowProc = startShell(nimdow)
+  sleep 200
+  sendEvents()
 
 setEnv("DISPLAY", ":1")
 
